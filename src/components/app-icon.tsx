21ba1ca22be9dade3b55b894f3c79992b28b823e
Davideacq <info@davideacq.com>
2025-07-22 16:18:40 +0200

    Sposta icone in assets/images e aggiorna i path per deploy Vercel

--- src/components/app-icon.tsx
+++ src/components/app-icon.tsx
@@ -2,15 +2,15 @@ import React from 'react';
 import { Image, ImageStyle, StyleProp } from 'react-native';
 
 const iconMap: Record<string, any> = {
-  check: require('../../dist/assets/images/check.png'),
-  bell: require('../../dist/assets/images/bell.png'),
-  eye: require('../../dist/assets/images/eye.png'),
-  'eye-off': require('../../dist/assets/images/eye-off.png'),
-  'arrow-left': require('../../dist/assets/images/arrow-left.png'),
-  home: require('../../dist/assets/images/home.png'),
-  settings: require('../../dist/assets/images/settings.png'),
-  user: require('../../dist/assets/images/user.png'),
-  'book-open': require('../../dist/assets/images/book-open.png'),
+  check: require('../../assets/images/check.png'),
+  bell: require('../../assets/images/bell.png'),
+  eye: require('../../assets/images/eye.png'),
+  'eye-off': require('../../assets/images/eye-off.png'),
+  'arrow-left': require('../../assets/images/arrow-left.png'),
+  home: require('../../assets/images/home.png'),
+  settings: require('../../assets/images/settings.png'),
+  user: require('../../assets/images/user.png'),
+  'book-open': require('../../assets/images/book-open.png'),
 };
 
 interface AppIconProps {
