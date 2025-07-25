386feb44808b32f8f16e2e0704d47151a87c1830
Davideacq <info@davideacq.com>
2025-07-25 09:56:07 +0200

    Add Vercel configuration and notification feature to Psychologist Dashboard

--- src/components/app-icon.tsx
+++ src/components/app-icon.tsx
@@ -2,15 +2,16 @@ import React from 'react';
 import { Image, ImageStyle, StyleProp } from 'react-native';
 
 const iconMap: Record<string, any> = {
-  check: require('../../dist/assets/images/check.png'),
-  bell: require('../../dist/assets/images/bell.png'),
-  eye: require('../../dist/assets/images/eye.png'),
-  'eye-off': require('../../dist/assets/images/eye-off.png'),
-  'arrow-left': require('../../dist/assets/images/arrow-left.png'),
-  home: require('../../dist/assets/images/home.png'),
-  settings: require('../../dist/assets/images/settings.png'),
-  user: require('../../dist/assets/images/user.png'),
-  'book-open': require('../../dist/assets/images/book-open.png'),
+  // check: require('../../dist/assets/images/check.png'), // non esiste
+  // bell: require('../../dist/assets/images/bell.png'), // non esiste
+  // eye: require('../../dist/assets/images/eye.png'), // non esiste
+  // 'eye-off': require('../../dist/assets/images/eye-off.png'), // non esiste
+  // 'arrow-left': require('../../dist/assets/images/arrow-left.png'), // non esiste
+  // home: require('../../dist/assets/images/home.png'), // non esiste
+  // settings: require('../../dist/assets/images/settings.png'), // non esiste
+  // user: require('../../dist/assets/images/user.png'), // non esiste
+  // 'book-open': require('../../dist/assets/images/book-open.png'), // non esiste
+  // Se vuoi aggiungere icone, mettile in assets/images e aggiorna qui i path
 };
 
 interface AppIconProps {
